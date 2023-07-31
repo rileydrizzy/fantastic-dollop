840f9c33e3b63f18e0fe300635a6bfe563c2d952
Ladipo Ipadeola <rileydrizzy@hotmail.com>
2023-07-31 23:06:01 +0100

    updates

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,12 +2,19 @@
 
 """
 
-import os
-import loguru
+from pathlib import Path
+from loguru import logger
 
 print("running okay")
 
-from loguru import logger
+Format_style = "{time:YYYY_MM_DD HH:mm:ss} | {level} | {module}: {function}{line} - {message}"
 
-logger.debug("Happy logging with Loguru!")
+log_dir = 'logs'
+log_filepath = Path(log_dir, "running_logs.log")
+Path.mkdir(log_dir, exist_ok= True)
 
+#os.makedirs(log_dir, exist_ok= True)
+
+logger.add(log_filepath,
+    format= Format_style,
+    level= "INFO",)
